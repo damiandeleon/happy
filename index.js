1606e507857ce121c10e87e74c91a5c927be20e9
damian.deleon <damianmdeleon@gmail.com>
2021-03-10 21:13:30 -0600

    updated the index.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000; //remember that PORT has to be in all caps!
+const PORT = process.env.PORT || 3000;
 
-return fs.readFile(path.join(__dirname, "index.html"), (e, data) => {
-    if (e) console.error(e);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  });
+app.get("/", function (req, res) {
+    return fs.readFile(path.join(__dirname, "index.html"), (e, data) => {
+        if (e) console.error(e);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+});
 
-app.listen(PORT, () => {console.log(`App listening on port ${PORT}`)});
+app.listen(PORT, () => { console.log(`App listening on port ${PORT}`) });
